3b3f2743926fa0c118dc1901f510c2e806cbd105
EricKwon <eric@gmail.com>
2025-09-09 12:34:13 +0900

    서버 CI 구축

---
+++ Server/Domain/User.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Domain;
+
+internal class User
+{
+    public bool Register(string email)
+    {
+        const string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+        return Regex.IsMatch(email, pattern);
+    }
+}
