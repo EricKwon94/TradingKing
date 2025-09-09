900ea238a175f9e0d5a1843b394fdd773f810e54
Eric Kwon <36413699+EricKwon94@users.noreply.github.com>
2025-09-09 12:48:50 +0900

    서버 CI 구축 (#1)
    * 서버 CI 구축
    
    * change name
    
    * d
    
    * 테스트 실패
    
    * Revert "테스트 실패"
    
    This reverts commit d2ec68539294e5eb8e16e742940cdacbe4484972.
    
    ---------
    
    Co-authored-by: EricKwon <eric@gmail.com>

---
+++ Server/Domain.Tests/UserTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+
+namespace Domain.Tests;
+
+public class UserTests
+{
+    [Theory]
+    [InlineData("asd2@asd.com")]
+    [InlineData("asd@asd.com")]
+    [InlineData("asd@asd.co.kr")]
+    public void email_is_valid(string email)
+    {
+        User sut = new User();
+        bool expectedResult = sut.Register(email);
+        expectedResult.Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData("asd2asd.com")]
+    [InlineData("asd@asdcom")]
+    public void email_is_not_valid(string email)
+    {
+        User sut = new User();
+        bool expectedResult = sut.Register(email);
+        expectedResult.Should().BeFalse();
+    }
+}
