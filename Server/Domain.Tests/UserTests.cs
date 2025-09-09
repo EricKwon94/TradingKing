54e58adec6aea21ac593de42f78ce865f15edd47
EricKwon <eric@gmail.com>
2025-09-09 13:25:16 +0900

    d

--- Server/Domain.Tests/UserTests.cs
+++ Server/Domain.Tests/UserTests.cs
@@ -22,6 +22,6 @@ public class UserTests
     {
         User sut = new User();
         bool expectedResult = sut.Register(email);
-        expectedResult.Should().BeTrue();
+        expectedResult.Should().BeTrue(); 2
     }
 }
