496776c7213487a2d083bd05e54d724c775586d3
EricKwon <gss5279@naver.com>
2025-09-18 11:07:36 +0900

    Scale out Test

---
+++ Server/Host/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Timers;
+
+namespace Host.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    public static int num = Random.Shared.Next();
+    public static Timer timer = new Timer(3000);
+
+    private readonly ILogger<TestController> _logger;
+
+    public TestController(ILogger<TestController> logger)
+    {
+        _logger = logger;
+        if (!timer.Enabled)
+        {
+            timer.Elapsed += (s, e) =>
+            {
+                _logger.LogInformation("{time}: Scale out test", e.SignalTime);
+            };
+            timer.Start();
+        }
+    }
+
+    [HttpGet]
+    public int Test1()
+    {
+        return num;
+    }
+}
