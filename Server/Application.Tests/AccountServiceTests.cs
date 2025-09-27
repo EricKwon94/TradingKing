504f23f20695ad018c016b517804863c524ec413
Eric Kwon <36413699+EricKwon94@users.noreply.github.com>
2025-09-27 17:05:16 +0900

    [server] DB를 테스트 및 CI 보강 (#24)

---
+++ Server/Application.Tests/AccountServiceTests.cs
@@ -0,0 +1,93 @@
+﻿using Application.Orchestrations;
+using Common;
+using FluentAssertions;
+using Infrastructure.EFCore;
+using Infrastructure.Persistences;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Application.Tests;
+
+public class AccountServiceTests : IClassFixture<TestDatabaseFixture>
+{
+    private readonly TestDatabaseFixture _fixture;
+    private const string ISSKEY = "x08z5dZcVX4QDJa3!QumT8?5y1Ezzp2bXjzHeDgzfR";
+
+    public AccountServiceTests(TestDatabaseFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Register_user()
+    {
+        // arrange
+        string id = "string1";
+        string pwd = "asdasd";
+        string expectedPwd = new Encryptor().Encrypt(pwd);
+        using var context = _fixture.CreateContext();
+        context.Database.BeginTransaction();
+        var transaction = new Transaction(context);
+        var repo = new UserRepository(context);
+
+        var sut = new AccountService(transaction, repo);
+
+        // act
+        bool result = await sut.RegisterAsync(id, pwd, default);
+
+        // assert
+        result.Should().BeTrue();
+        var user = await context.Users.FirstAsync(e => e.Id == id);
+        user.Id.Should().Be(id);
+        user.Password.Should().Be(expectedPwd);
+        user.Jwt.Should().BeNull();
+    }
+
+    [Fact]
+    public async Task Can_not_register_user_if_already_exist()
+    {
+        // arrange
+        string id = "string1";
+        string pwd = "asdasd";
+        using var context = _fixture.CreateContext();
+        context.Database.BeginTransaction();
+        var transaction = new Transaction(context);
+        var repo = new UserRepository(context);
+
+        var sut = new AccountService(transaction, repo);
+        bool r = await sut.RegisterAsync(id, pwd, default);
+        Assert.True(r);
+
+        // act
+        bool result = await sut.RegisterAsync(id, pwd, default);
+
+        // assert
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task Can_login_if_user_exist()
+    {
+        // arrange
+        string id = "string1";
+        string pwd = "asdasd";
+        using var context = _fixture.CreateContext();
+        context.Database.BeginTransaction();
+        var transaction = new Transaction(context);
+        var repo = new UserRepository(context);
+
+        var sut = new AccountService(transaction, repo);
+
+        bool r = await sut.RegisterAsync(id, pwd, default);
+        Assert.True(r);
+        context.ChangeTracker.Clear();
+
+        // act
+        string? expectedJwt = await sut.LoginAsync(id, pwd, ISSKEY, "iss", "aud", default);
+
+        // assert
+        expectedJwt.Should().NotBeNullOrEmpty();
+        var user = await context.Users.FirstAsync(e => e.Id == id);
+        user.Jwt.Should().Be(expectedJwt);
+    }
+}
---
+++ Server/Application.Tests/TestDatabaseFixture.cs
@@ -0,0 +1,52 @@
+﻿using Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+
+namespace Application.Tests;
+
+public class TestDatabaseFixture
+{
+    private const string WINDOWS = "Server=(localdb)\\MSSQLLocalDB;Database=TradingKingTests;";
+    private const string OTHERES = "Server=localhost,1433;Database=TradingKingTests;User Id=sa;Password=Strongsanklfj2045!@$#%jsjS;TrustServerCertificate=True";
+
+    private static readonly Lock _lock = new();
+    private static bool _databaseInitialized;
+
+    public TestDatabaseFixture()
+    {
+        lock (_lock)
+        {
+            if (!_databaseInitialized)
+            {
+                using var context = CreateContext();
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+                _databaseInitialized = true;
+            }
+        }
+    }
+
+    internal TradingKingContext CreateContext()
+    {
+        DbContextOptionsBuilder<TradingKingContext> builder;
+
+        if (OperatingSystem.IsWindows())
+        {
+            builder = new DbContextOptionsBuilder<TradingKingContext>()
+                .UseSqlServer(WINDOWS)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+        else
+        {
+            builder = new DbContextOptionsBuilder<TradingKingContext>()
+                .UseSqlServer(OTHERES)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        return new TradingKingContext(builder.Options);
+    }
+}
