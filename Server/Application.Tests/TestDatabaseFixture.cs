e8ac2c45a25721dd629adf800be05a7766c372f2
EricKwon <gss5279@naver.com>
2025-09-27 14:58:46 +0900

    [server] DB를 테스트합니다.

---
+++ Server/Application.Tests/TestDatabaseFixture.cs
@@ -0,0 +1,41 @@
+﻿using Infrastructure.EFCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+
+namespace Application.Tests;
+
+public class TestDatabaseFixture
+{
+    private const string ConnectionString = "Server=(localdb)\\MSSQLLocalDB;Database=TradingKingTests;";
+
+    private static readonly Lock _lock = new();
+    private static bool _databaseInitialized;
+
+    public TestDatabaseFixture()
+    {
+        lock (_lock)
+        {
+            if (!_databaseInitialized)
+            {
+                using (var context = CreateContext())
+                {
+                    context.Database.EnsureDeleted();
+                    context.Database.EnsureCreated();
+                }
+                _databaseInitialized = true;
+            }
+        }
+    }
+
+    internal TradingKingContext CreateContext()
+    {
+        var builder = new DbContextOptionsBuilder<TradingKingContext>()
+            .UseSqlServer(ConnectionString)
+            .EnableSensitiveDataLogging()
+            .LogTo(Console.WriteLine, LogLevel.Information);
+
+        return new TradingKingContext(builder.Options);
+    }
+}
