b58b0f9e5bc47baf204082b8987373089c28cd6a
EricKwon <gss5279@naver.com>
2025-09-28 21:45:01 +0900

    d

--- Server/IntegrationTests/AccountControllerTests.cs
+++ Server/IntegrationTests/AccountControllerTests.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using Xunit.Abstractions;
 
 namespace IntegrationTests;
 
@@ -13,14 +14,16 @@ public class AccountControllerTests : IClassFixture<CustomWebApplicationFactory<
 {
     private readonly HttpClient _client;
     private readonly CustomWebApplicationFactory<Program> _factory;
+    private readonly ITestOutputHelper _output;
 
-    public AccountControllerTests(CustomWebApplicationFactory<Program> factory)
+    public AccountControllerTests(CustomWebApplicationFactory<Program> factory, ITestOutputHelper output)
     {
         _factory = factory;
         _client = factory.CreateClient(new WebApplicationFactoryClientOptions
         {
             AllowAutoRedirect = false
         });
+        _output = output;
     }
 
     [Fact]
@@ -68,6 +71,7 @@ public class AccountControllerTests : IClassFixture<CustomWebApplicationFactory<
         // assert
         string body = await res.Content.ReadAsStringAsync();
         System.Console.WriteLine(body);
+        _output.WriteLine(body);
         body.Should().Be("-1");
         res.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
