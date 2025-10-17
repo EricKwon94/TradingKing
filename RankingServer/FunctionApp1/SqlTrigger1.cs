aba7eaf75eafefc1369c2fa8f2f4200ae352938d
EricKwon <gss5279@naver.com>
2025-10-17 10:53:24 +0900

    [server] 컨테이너화

--- RankingServer/FunctionApp1/SqlTrigger1.cs
+++ RankingServer/FunctionApp1/SqlTrigger1.cs
@@ -1,12 +1,8 @@
-using Azure.Messaging.EventHubs;
-using Azure.Messaging.EventHubs.Producer;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace FunctionApp1;
 
@@ -14,6 +10,7 @@ public class SqlTrigger1
 {
     private readonly ILogger _logger;
     private readonly EventHubFactory _hubFactory;
+    private const string CS = "Data Source=tradingking-dev-koreacentral-01.database.windows.net;Initial Catalog=TradingKing;User ID=TCdK8WpDqmknFQVpB7Dev;Password=QM42QSCU^BvQ25mM@0LgDev;";
 
     public SqlTrigger1(ILoggerFactory loggerFactory, EventHubFactory hubFactory)
     {
@@ -23,11 +20,16 @@ public class SqlTrigger1
 
     // Visit https://aka.ms/sqltrigger to learn how to use this trigger binding
     [Function("SqlTrigger1")]
-    public async Task Run(
+    public void Run(
         [SqlTrigger("[dbo].[Orders]", "TradingKing")] IReadOnlyList<SqlChange<Order>> changes,
             FunctionContext context)
     {
-        await using EventHubProducerClient hub = _hubFactory.CreateSqlHub();
+        foreach (SqlChange<Order> change in changes)
+        {
+            _logger.LogInformation("{item}", change.Item);
+        }
+
+        /*await using EventHubProducerClient hub = _hubFactory.CreateSqlHub();
 
         EventDataBatch batch = await hub.CreateBatchAsync(context.CancellationToken);
 
@@ -38,7 +40,7 @@ public class SqlTrigger1
             batch.TryAdd(data);
         }
 
-        await hub.SendAsync(batch, context.CancellationToken);
+        await hub.SendAsync(batch, context.CancellationToken);*/
     }
 }
 
--- RankingServer/FunctionApp1/TimerTrigger1.cs
+++ RankingServer/FunctionApp1/TimerTrigger1.cs
@@ -1,9 +1,6 @@
-using Azure.Messaging.EventHubs;
-using Azure.Messaging.EventHubs.Producer;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
 
 namespace FunctionApp1;
 
@@ -11,25 +8,37 @@ public class TimerTrigger1
 {
     private readonly ILogger _logger;
     private readonly EventHubFactory _hubFactory;
+    private readonly IConfiguration _config;
 
-    public TimerTrigger1(ILoggerFactory loggerFactory, EventHubFactory hubFactory)
+    public TimerTrigger1(ILoggerFactory loggerFactory, EventHubFactory hubFactory, IConfiguration config)
     {
         _logger = loggerFactory.CreateLogger<TimerTrigger1>();
         _hubFactory = hubFactory;
+        _config = config;
     }
 
 
     [Function("TimerTrigger1")]
-    public async Task Run(
-      [TimerTrigger("*/50 * * * * *")] TimerInfo myTimer, FunctionContext context)
+    public void Run(
+      [TimerTrigger("*/10 * * * * *")] TimerInfo myTimer, FunctionContext context)
     {
-        _logger.LogInformation("타이머트리거 {time}", DateTime.UtcNow);
+        string? eventHubCs = _config["EventHub"];
+        string? tradingking = _config.GetConnectionString("TradingKing");
+        string? sqlHubName = _config["SqlHubName"];
+        string? timerHubName = _config["TimerHubName"];
+
+        _logger.LogInformation("cs: {cs}", eventHubCs);
+        _logger.LogInformation("tradingking: {tradingking}", tradingking);
+        _logger.LogInformation("name1: {sqlHubName}", sqlHubName);
+        _logger.LogInformation("name2: {timerHubName}", timerHubName);
+
+        /*_logger.LogInformation("타이머트리거 {time}", DateTime.UtcNow);
 
         await using EventHubProducerClient hub = _hubFactory.CreateSqlHub();
 
         EventDataBatch batch = await hub.CreateBatchAsync(context.CancellationToken);
         var data = new EventData($"Hello I am Functions {DateTime.UtcNow}");
         batch.TryAdd(data);
-        await hub.SendAsync(batch, context.CancellationToken);
+        await hub.SendAsync(batch, context.CancellationToken);*/
     }
 }
