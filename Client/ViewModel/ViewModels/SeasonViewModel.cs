65081ea545ed8994212dbda7cdf68310c34e0281
EricKwon <gss5279@naver.com>
2025-10-22 12:05:01 +0900

    [client] 시즌 정보를 보여줍니다.

--- Client/ViewModel/ViewModels/SeasonViewModel.cs
+++ Client/ViewModel/ViewModels/SeasonViewModel.cs
@@ -1,5 +1,10 @@
 ﻿using Application.Gateways;
+using Application.Services;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,16 +13,52 @@ namespace ViewModel.ViewModels;
 public partial class SeasonViewModel : BaseViewModel
 {
     private readonly ILogger<SeasonViewModel> _logger;
+    private readonly IAlertService _alert;
     private readonly ISeasonApi _season;
 
-    public SeasonViewModel(ILogger<SeasonViewModel> logger, ISeasonApi season)
+    public ObservableCollection<SeasonModel> Seasons { get; } = [];
+
+    public SeasonViewModel(ILogger<SeasonViewModel> logger, IAlertService alert, ISeasonApi season)
     {
         _logger = logger;
+        _alert = alert;
         _season = season;
     }
 
     public override async Task LoadAsync(CancellationToken ct)
     {
-        var v = await _season.GetSeasonsAsync(ct);
+        List<ISeasonApi.SeasonRes> seasons;
+        try
+        {
+            seasons = [.. await _season.GetSeasonsAsync(ct)];
+        }
+        catch (Exception ex)
+        {
+            await _alert.DisplayAlertAsync("error", ex.Message, "ok", ct);
+            return;
+        }
+
+        for (int i = 0; i < seasons.Count - 1; i++)
+        {
+            Seasons.Add(new SeasonModel(i + 1, seasons[i].StartedAt.ToLocalTime()));
+        }
+    }
+}
+
+public partial class SeasonModel
+{
+    public int Seq { get; }
+    public DateTime StartedAt { get; }
+
+    public SeasonModel(int seq, DateTime startedAt)
+    {
+        Seq = seq;
+        StartedAt = startedAt;
+    }
+
+    [RelayCommand]
+    public async Task Click(CancellationToken ct)
+    {
+
     }
 }
